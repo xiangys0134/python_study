9eaa04e1ec5294f95f9624af6befe2e5865ec238
Your Name <you@example.com>
2018-09-04 08:35:58 +0800

    systems scrips

--- day3/ttttt.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-s = "123a4b5c"
-s1 = s[3:-2]
-print(s1)
--- day3/uuu.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-li = ["TaiBai ", "alexC", "AbC ", "egon", " riTiAn", "WuSir", "  aqc"]
-tb1 = []
-for i in  li:
-    i = i.strip()
-    if (i.startswith('A') or i.startswith('a')) and i.endswith('c'):
-        tb1.append(i)
-
-print(tb1)
