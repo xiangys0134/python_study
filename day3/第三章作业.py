b94a66f9c44c174ab1e9dffca7b22c0a6f1fdb05
DESKTOP-3OKFSTI\xiangys0134 <250919938@qq.com>
2018-09-06 23:53:23 +0800

    python第三章练习

--- day3/第三章作业.py
+++ day3/第三章作业.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-#yousong.xiang 2018.9.4
+#yousong.xiang 2018.9.6
 #v1.0.2
 #修改字典参数，添加对应的备注，修复bug，友好打印
-#预先需要两个文件 users.txt、session
+#预先需要创建两个文件 users1.txt、session(users1.txt 存储用户数据 session存储用户session)
+#业务逻辑：
+#简介：分用户注册模块、用户登陆模块、用户充值模块、用户模块 #注：用户购物逻辑沿用第二章编程,无大改动，比较混乱
+#1.分别打印出登陆、注册、购物、退出
+#2.登陆：用户登陆时进行session判断,如果重复登陆则返回重复登陆信息
+#3.用户注册：用户进行注册时查询用户信息文件,如果已有用户存在则提示改用户已存在
+#3.购物车：程序运行时如无用户登陆状态则提示需要先进行用户登陆,之后进行充值,购物车操作
+#4.打印购物信息表
+
 
 import os
 #商品字典
@@ -18,30 +26,53 @@ a ={
 
 select_user = ['登录','注册','购物','退出']
 #用户信息保存文件,文件存储格式 'user password'
-users_file = 'users.txt'    #用户账号密码文件
+users_file = 'users1.txt'    #用户账号密码文件
 session_file = 'session'    #session文件包含用户登录信息
 
-def loggin(user,password):  #用户登陆函数
-    with open(users_file,encoding='utf-8') as f_user:
-        for i in f_user:
-            if i == '':
-                continue
-            user_info = i.split()
-            ##去除空格
-            if user.strip().lower() == user_info[0].strip().lower() and password.strip() == user_info[1].strip():
+if os.path.exists(session_file):
+    with open(session_file,'w',encoding='utf-8') as f1:
+        f1.write('')    #程序开始运行则将session默认进行清空
+
+
+def loggin(user,password):  #用户登陆函数,登陆状态信息0 表示登陆失败 1表示登陆成功 2表示之前已登陆过该账户 3表示登陆失败
+    #with open(users_file,'r',encoding='utf-8') as f_user:   #打开用户信息文件users_file
+    f_user = open(users_file,'r',encoding='utf-8')
+    for i in f_user:
+        user_info = i.split()
+        #print(user_info[0],user_info[1])
+        ##去除空格
+        if user.strip().lower() == user_info[0].strip().lower() and password.strip() == user_info[1].strip():
+            f_login = open(session_file,'r',encoding='utf-8') #读取session文件中的值
+            seesion_user = f_login.read()
+            f_login.close()
+            if user.strip() != seesion_user.strip():
+                f1 = open(session_file,'w',encoding='utf-8')
+                f1.write(user.strip())
+                f1.close()
                 return 1
+            else:
+                return 2
+    else:
+        return 3
+    f_user.close()
+    return 0    #如果文件无内容则返回0
 
 def register(user,password):    #0 表示用户已存在 1表示用户注册成功 2表示用户注册时不能使用特殊字符
-    with open(users_file,encoding='utf-8') as f_user:
+    if os.path.exists(users_file):
+        f_user =  open(users_file,'r',encoding='utf-8')
         for i in f_user:
             user_info = i.split()
             if user.strip().lower() == user_info[0].strip().lower():    #判断用户是否存在,存在则返回0
                 return 0
-    if str(user).isalnum() == False or str(password).isalnum() == False:    #账号密码不能为特殊字符
-        return 2
+        f_user.close()
 
-    with open(users_file,'a',encoding='utf-8') as f_user:
+        if str(user).isalnum() == False or str(password).isalnum() == False:    #账号密码不能为特殊字符
+            return 2
+
+        f_user = open(users_file,'a',encoding='utf-8')
+        #with open(users_file,'a',encoding='utf-8') as f_user:
         f_user.write(user.strip() + '\t' + password.strip() +'\n')
+        f_user.close()
         return 1
 
 
@@ -118,35 +149,23 @@ while flag:
                     password = input('请输入你的密码：')
                     #对用户进行判断
                     result = loggin(user,password)
+                    #print(result)
                     if result == 1:
-                        #print('用户登录成功')
-                        if os.path.exists(session_file):    #情况session用户登录状态
-                            f1 = open(session_file,'r',encoding='utf-8')
-                            f1_id = f1.readlines()
-                            f1.close()
-                            if f1_id[0] == user:
-                                print('用户：%s 已处于登录状态中...'%f1_id[0])
-                                count = 4
-                            # else:
-                            #     #os.remove(session_file)
-                            #     with open(session_file, 'w', encoding='utf-8') as f2:
-                            #         f2.write(user)
-                            #     count = 5
-                        else:
-                            os.remove(session_file)
-                            with open(session_file,'w',encoding='utf-8') as f1:
-                                f1.write(user)
-                            count = 5
-                        print('用户登录成功')
-
-
-                        #flag = False
-                        #break
-                    else:
-                        print('用户登录失败,请重新登录')
+                        print('用户:%s登陆成功'%(user.strip()))
+                        count = 5
+                    elif result == 0:
+                        print('用户不存在,请注册')
+                        count = 5
+                    elif result == 3:
+                        print('用户登陆失败,请重新输入账户密码')
                         count += 1
                         if count >3:
-                            print('登录次数超过3次')
+                            print('\033[0;31m登录次数超过3次\033[0m')
+                    elif result == 2:
+                        print('用户\033[0;31m%s\033[0m已经登陆,无需重复登陆'%(user.strip()))
+                        count = 5
+                    else:
+                        pass
 
             #tb1 = {}
             if int(client_select_id) == 1:   #用户注册
@@ -155,14 +174,15 @@ while flag:
                     user = input('注册账号：')
                     password = input('注册密码：')
                     result = register(user,password)
+                    #print(result)
                     if result == 0:
                         print('用户已存在,请重新注册!')
                     elif result == 2:
                         print('请使用字母大小写及数字')
                     elif result == 1:
-                        print('用户注册成功')
-                        if os.path.exists(session_file):    #情况session用户登录状态
-                            os.remove(session_file)
+                        print('用户:%s注册成功'%(user.strip()))
+                        # if os.path.exists(session_file):    #情况session用户登录状态
+                        #     os.remove(session_file)
                         with open(session_file,'w',encoding='utf-8') as f1:
                             f1.write(user)
                         fix = False
@@ -170,8 +190,8 @@ while flag:
                         pass
 
             if int(client_select_id) == 2:  #用户购物
-                if os.path.exists(session_file) == False:
-                    print('请先登录')
+                # if os.path.exists(session_file) == False:
+                #     print('请先登录')
 
                 with open(session_file,'r',encoding='utf-8') as f2: #读取用户session
                     file_session = f2.read()
@@ -230,15 +250,17 @@ while flag:
                             else:
                                 print('商品：\033[0;31m%s\033[0m无法进行购买,余额：\033[0;31m %s \033[0m' % (a[goods][0], l1[-1]))
 
-                #无法通过seesion功能将注册或登录的用户进行
                     else:
-                        print('用户未登录,请返回进行登录')
+                        print('\033[1;35m请先登陆\033[0m')
+
 
             if int(client_select_id) == 3:  #退出
                 print('退出')
-                if os.path.exists(session_file):
-                    os.remove(session_file)
                 break
+        else:
+            print('\033[1;35m输入序号有误,请重新输入!\033[0m')
+    else:
+        print('\033[1;35m输入字符异常,请重新输入!!!\033[0m')
 
 
 
