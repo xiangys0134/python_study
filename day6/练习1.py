2a28783c9efd4f6d318981f3a31edae57ef557ad
Your Name <you@example.com>
2018-10-25 09:44:50 +0800

    dddd

--- day6/练习1.py
+++ day6/练习1.py
@@ -1,5 +1,8 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import datetime
+
 '''
 安装dateutil模块
 >pip install python-dateutil
@@ -8,7 +11,9 @@
 >>> a = datetime(2012, 9, 23)
 >>> a + relativedelta(months=+1)
 datetime.datetime(2012, 10, 23, 0, 0)
-
 '''
 
-from dateutil.relativedelta import relativedelta
+t2 = datetime.datetime(2017, 4, 16, 21, 21, 20)
+
+# t1 = datetime(2018,1,21,12,23,12)
+print(t2)
