aee531a2b88ecb88bbf4961db1f4b99f302b63d5
DESKTOP-3OKFSTI\xiangys0134 <250919938@qq.com>
2018-10-31 00:23:24 +0800

    第六章练习

---
+++ day7/作业01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#author:yousong.xiang 2018.10.31
+#v1.0.1
+#学生选课系统
+
+'''
+1.通过文件存储两张表(成功信息表,课程表)；学生表："alex,123,student,java,python"，课程表：'java,c++,python,c#'
+2.创建两个类分别为学生类，管理员类
+'''
+
+#预先生成数据文件
