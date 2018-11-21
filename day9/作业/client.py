aded890a3351a0bdd3dd056a0375268359ebb60e
DESKTOP-3OKFSTI\xiangys0134 <250919938@qq.com>
2018-11-21 23:54:46 +0800

    第10天

--- day9/作业/client.py
+++ day9/作业/client.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import socket
+
+class Myclient:
+    def __init__(self):
+        self.sk = socket.socket()
+        self.sk.connect(('127.0.0.1',8000))
+
+    def mysend(self,dic):
+        self.sk.send(dic.encode('utf8'))
+        # msg = self.sk.recv(1024)
+        # print(msg.decode('utf8'))
+
+    def myrecv(self):
+        msg = self.sk.recv(1024)
+        # print(msg)
+        return msg
+
+
+# t1 = {'username':'alex','pwd':'123','job':'user'}
+t1 = 'aaa'
+myclient = Myclient()
+myclient.mysend(t1)
+msg = myclient.myrecv()
+print(msg)
--- day9/作业/server.py
+++ day9/作业/server.py
@@ -9,10 +9,25 @@ import os
 
 server = socket.socket()
 server.bind(('127.0.0.1',8000))
-server.bind(5)
+server.listen(5)
+
 
 class Myserver:
-    def __init__(self):
-        pass
+    def __init__(self,server):
+        self.conn,self.addr = server.accept()
 
-    while True:
+
+    def myrecv(self):
+        msg = self.conn.recv(1024)
+        # print(msg.decode('utf8'))
+        return msg.upper()
+
+    def mysend(self,msg):
+        self.conn.send(msg.encode('utf8'))
+
+
+while True:
+    a = Myserver(server)
+    msg = a.myrecv()
+    print(msg)
+
