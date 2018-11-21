aa9ec4da13cc8e91e2cae6e24130000211e8c407
xiangys0134 <you@example.com>
2018-11-21 21:43:40 +0800

    第10天

--- day9/作业/server.py
+++ day9/作业/server.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-#
+from multiprocessing import Process
+import os
+import socket
+import time
+import os
+
+server = socket.socket()
+server.bind(('127.0.0.1',8000))
+server.bind(5)
+
+class Myserver:
+    def __init__(self):
+        pass
+
+    while True:
