7716130b644475a7204da7c011847cd5b81d56f3
DESKTOP-3OKFSTI\xiangys0134 <250919938@qq.com>
2018-11-25 00:30:21 +0800

    第10天练习

--- day9/作业02/client.py
+++ day9/作业02/client.py
@@ -6,11 +6,12 @@ import struct
 import socket
 import json
 
-client = socket.socket()
-server_ip = ('127.0.0.1',8000)
+# client = socket.socket()
+# server_ip = ('127.0.0.1',8000)
 class Myclient:
-    def __init__(self,ip):
-        self.sk = client.connect(ip)
+    def __init__(self):
+        self.sk = socket.socket()
+        self.sk.connect(('127.0.0.1',8000))
 
     def mysend(self,msg):
         self.sk.send(msg)
@@ -20,39 +21,64 @@ class Myclient:
 
     def stuck_unpack(self):
         msg = self.sk.myrecv(4)
-        if not msg:return 'error'
+        if not msg: return 'error'
         dic_byte = struct.unpack('i', msg)[0]
-        byte_msg = msg.decode('utf8')
-        #dic = { 'name1':{'filename':'filename','byte_size':byte_size,'md5':md5}}
-        str_msg = json.loads(byte_msg)
+        byte_msg = self.sk.myrecv(dic_byte)
+        str_dic = byte_msg.decode('utf8')
+        # dic = { 'filename':'filename','byte_size':byte_size,'md5':md5}
+        str_msg = json.loads(str_dic)
         return str_msg
         # get_size = str_msg['name']['byte_size']
 
     def stuck_pack(self,dic):
-        """
-        dic = {
-          'file_name1':{'filename':'filename','byte':byte_size,'md5':md5}
-          'file_name2':{'filename':'filename','byte_size':byte_size,'md5':md5}
-        }
-        """
         if not dic:return 'error'
+        print(dic)
         byte_dic = json.dumps(dic).encode('utf8')
-        dic_pak = struct.pack(byte_dic)
+        # print(byte_dic)
+        dic_pak = struct.pack('i',len(byte_dic))
+        print(dic_pak)
         if hasattr(self,'mysend'):
+            # print('aaaa')
             func = getattr(self,'mysend')
             func(dic_pak)
-
+            func(byte_dic)
+        else:
+            print('error')
 
     def conn_close(self):
         self.sk.close()
 
-
 if __name__ == '__main__':
-    client_socket = Myclient(server_ip)
+    client_socket = Myclient()
+    dic = {}
+    while True:
+        tmp_dir = input(r'请输入上传路径：')
+        if not tmp_dir:break
+        if os.path.exists(tmp_dir):
+            dic['filename'] = os.path.basename(tmp_dir)
+            dic['file_size'] = os.path.getsize(tmp_dir)
+
+        #将字典传给服务端
+        client_socket.stuck_pack(dic)
+
+        #开始传送数据
+        m = hashlib.md5()
+        with open(tmp_dir,'rb') as f:
+            for line in f:
+                client_socket.mysend(line)
+                print(len(line),'&&&&')
+                m.update(line)
+
+        md5_id = m.hexdigest()
+        print(md5_id)
+        dic['md5'] = md5_id
+        #再次将md5以字典的形式传给服务端
+        client_socket.stuck_pack(dic)
 
 
-    client.close()
-    # def stuck_unpack(sk,msg):
+    client_socket.conn_close()
+        # def stuck_unpack(sk,msg)
+    client_socket.conn_close()
 
 
 
--- day9/作业02/server.py
+++ day9/作业02/server.py
@@ -6,34 +6,42 @@ import struct
 import socket
 import json
 
+dir = os.path.dirname(os.path.abspath(__name__))
+
+if not os.path.exists(os.path.join(dir,'ftpdata')):
+    ftp_dir = os.path.join(dir,'ftpdata')
+    os.mkdir(ftp_dir)
+else:
+    ftp_dir = os.path.join(dir, 'ftpdata')
+
+service = socket.socket()
+service.bind(('127.0.0.1',8000))
+service.listen(5)
+
 class Myserver:
     def __init__(self,sk):
         self.conn,self.addr = sk.accept()
 
     def mysend(self, msg):
-        self.sk.send(msg)
+        self.conn.send(msg)
 
     def myrecv(self, buffer=1024):
-        return self.sk.recv(buffer)
+        return self.conn.recv(buffer)
 
     def stuck_unpack(self):
-        """"""
-        msg = self.sk.myrecv(4)
+        msg = self.myrecv(4)
+        print(msg)
         if not msg: return 'error'
         dic_byte = struct.unpack('i', msg)[0]
-        byte_msg = msg.decode('utf8')
-        # dic = { 'name1':{'filename':'filename','byte_size':byte_size,'md5':md5}}
-        str_msg = json.loads(byte_msg)
+        print(dic_byte,'长度')
+        byte_msg = self.myrecv(dic_byte)
+        str_dic = byte_msg.decode('utf8')
+        # dic = { 'filename':'filename','byte_size':byte_size,'md5':md5}
+        str_msg = json.loads(str_dic)
         return str_msg
         # get_size = str_msg['name']['byte_size']
 
     def stuck_pack(self, dic):
-        """
-        dic = {
-          'file_name1':{'filename':'filename','byte':byte_size,'md5':md5}
-          'file_name2':{'filename':'filename','byte_size':byte_size,'md5':md5}
-        }
-        """
         if not dic: return 'error'
         byte_dic = json.dumps(dic).encode('utf8')
         dic_pak = struct.pack(byte_dic)
@@ -43,4 +51,45 @@ class Myserver:
 
 
     def conn_close(self):
-        self.sk.close()
+        self.conn.close()
+
+if __name__ == '__main__':
+    while True:
+        sk = Myserver(service)
+        while True:
+            # try:
+                #获取客户端字典
+                msg_dic = sk.stuck_unpack()
+                print(msg_dic,'################')
+                data_recv = b''
+                data_leng = 0
+                m = hashlib.md5()
+                with open(os.path.join(ftp_dir,msg_dic['filename']),'wb') as f:
+                    while data_leng < msg_dic['file_size']:
+                        if msg_dic['file_size'] < 1024:
+                            data = sk.myrecv(msg_dic['file_size'])
+                            data_leng += len(data)
+                        else:
+                            data = sk.myrecv(1024)
+                            data_recv += data
+                            data_leng += len(data)
+                        f.write(data)
+                        f.seek(data_leng)
+                        print(data_leng,'&&&&&&')
+                        m.update(data)
+                    md5 = m.hexdigest()
+                    print(md5)
+                print('aaaa')
+                #再次获取客户端字典
+                new_dic = sk.stuck_unpack()
+                print(new_dic)
+                if md5 == new_dic['md5']:
+                    print('md5',md5)
+                else:
+                    print('md5值不一致，上传失败')
+            # except Exception:
+            #     print('cccccccccc')
+            #     sk.conn_close()
+            #     break
+
+    service.close()
